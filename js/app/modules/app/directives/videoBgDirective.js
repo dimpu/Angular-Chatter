72aa3719d1a64b6d7b3da1dda777483321b168b0
aravind buddha <buddhaaravind@gmail.com>
2014-12-03 18:18:45 +0530

    Updates

--- js/app/modules/app/directives/videoBgDirective.js
+++ js/app/modules/app/directives/videoBgDirective.js
@@ -5,7 +5,7 @@
 	define([],function(){
 		var videoBgDirective=function(){
 			 return {
-			    templateUrl: 'js/app/modules/app/views/partials/_video_bg.html',
+			    templateUrl: '/js/app/modules/app/views/partials/_video_bg.html',
 			    compile: function(scope,element,attrs){
 			    	return {
 			    		post:function(){
