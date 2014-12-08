6b01878c696d1859649bc6f5d6194b12e4926c9f
aravind buddha <buddhaaravind@gmail.com>
2014-12-08 13:38:39 +0530

    Updates

--- js/vendor.js
+++ js/vendor.js
@@ -13580,16 +13580,18 @@ return jQuery;
  */
 
 (function (factory) {
-    if ( typeof define === 'function' && define.amd ) {
-        // AMD. Register as an anonymous module.
-        define(['jquery'], factory);
-    } else if (typeof exports === 'object') {
-        // Node/CommonJS style for Browserify
-        module.exports = factory;
-    } else {
-        // Browser globals
-        factory(jQuery);
-    }
+    // if ( typeof define === 'function' && define.amd ) {
+    //     // AMD. Register as an anonymous module.
+    //     define(['jquery'], factory);
+    // } else if (typeof exports === 'object') {
+    //     // Node/CommonJS style for Browserify
+    //     module.exports = factory;
+    // } else {
+    //     // Browser globals
+    //     factory(jQuery);
+    // }
+
+    factory(jQuery);
 }(function ($) {
 
     var toFix  = ['wheel', 'mousewheel', 'DOMMouseScroll', 'MozMousePixelScroll'],
@@ -13779,16 +13781,18 @@ return jQuery;
 (function (factory) {
   'use strict';
 
-  if (typeof define === 'function' && define.amd) {
-    // AMD. Register as an anonymous module.
-    define(['jquery'], factory);
-  } else if (typeof exports === 'object') {
-    // Node/CommonJS
-    factory(require('jquery'));
-  } else {
-    // Browser globals
-    factory(jQuery);
-  }
+  // if (typeof define === 'function' && define.amd) {
+  //   // AMD. Register as an anonymous module.
+  //   define(['jquery'], factory);
+  // } else if (typeof exports === 'object') {
+  //   // Node/CommonJS
+  //   factory(require('jquery'));
+  // } else {
+  //   // Browser globals
+  //   factory(jQuery);
+  // }
+
+  factory(jQuery);
 }(function ($) {
   'use strict';
 
